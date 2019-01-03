ae8e01ed04e5129949de56077a5df81f7e46a99a
Farhan <farhansghazi@outlook.com>
2019-01-03 11:43:13 +1100

    moved func defs to maintain consistency

--- Stack/Stack.h
+++ Stack/Stack.h
@@ -13,48 +13,8 @@ typedef struct StackRep {
 
 } StackRep;
 
-Stack InitializeStack (int data) {
-
-	Stack new_stack = malloc(sizeof(StackRep));
-	new_stack->current_index = 0;
-	new_stack->top_elem_index = MAX_ITEMS;
-	new_stack->elems[new_stack->current_index] = data;
-	return new_stack;
-
-}
-
-Stack Push (Stack stack , int data) {
-
-	if(stack->current_index < MAX_ITEMS) {
-		stack->current_index++;
-		stack->top_elem_index = stack->current_index;
-		stack->elems[stack->current_index] = data;
-		return stack; 
-	} else {
-		printf("Stack is full!");
-	}
-	return stack;
-}
-
-Stack Pop (Stack stack) {
-
-	stack->elems[stack->top_elem_index] = 0;
-	stack->top_elem_index--; 
-	return stack;
-}
-
-void DisplayStack (Stack stack) {
-
-	printf("\nTOP : ");
-	for(int i = stack->top_elem_index; i > 0; i--){
-		printf("%d " , stack->elems[i]);
-	}
-	printf(": TAIL\n");
-
-	printf("Top of stack : %d\n\n" , stack->elems[stack->top_elem_index]);
-
-}
-
-void FreeStack (Stack stack) {
-	free(stack);
-}
+Stack InitializeStack (int data) ;
+Stack Push 			  (Stack stack , int data);
+Stack Pop 			  (Stack stack);
+void  DisplayStack 	  (Stack stack);
+void  FreeStack 	  (Stack stack);
